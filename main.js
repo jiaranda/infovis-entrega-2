0356b6522d3e09cf49b0f15a87ea42c95cf2cbd4
Javier <jiaranda@uc.cl>
2020-11-01 13:04:29 -0300

    ♻️ refactor zoom

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
 const main = async () => {
   const listingsData = await loadCSV("data/listings.csv");
   const neighborhoodsGeoData = await loadJSON("data/neighborhoods.geojson");
-  const mapContainer = d3.select("#map");
   const mapSVG = await createMap(neighborhoodsGeoData);
-
-  let zoom = d3
-    .zoom()
-    .extent([
-      [0, 0],
-      [500, 500],
-    ])
-    .translateExtent([
-      [-100, -100],
-      [500 + 100, 500 + 100],
-    ])
-    .scaleExtent([1, 12])
-    .on("zoom", (event) => {
-      mapSVG.select("#mapGroup").attr("transform", event.transform);
-    });
-
-  mapContainer.call(zoom);
 };
 
 main();
--- map.js
+++ map.js
@@ -1,19 +1,25 @@
 const createMap = async (geoData) => {
-  const projection = d3.geoMercator().fitSize(["500", "500"], geoData);
+  const width = 500;
+  const height = 500;
+  const projection = d3.geoMercator().fitSize([width, height], geoData);
   const geoPaths = d3.geoPath().projection(projection);
+  const mapContainer = d3
+    .select("#map")
+    .style("height", `${height}px`)
+    .style("width", `${width}px`);
 
   const svg = d3
     .select("#map")
     .append("svg")
-    .attr("width", "500")
-    .attr("height", "500");
+    .attr("width", width)
+    .attr("height", height);
 
   svg
     .append("clipPath")
     .attr("id", "clip")
     .append("rect")
-    .attr("width", "500")
-    .attr("height", "500");
+    .attr("width", width)
+    .attr("height", height);
 
   const mapGroup = svg
     .append("g")
@@ -29,6 +35,23 @@ const createMap = async (geoData) => {
     .attr("fill", "blue")
     .attr("opacity", 0.3)
     .attr("stroke", "blue");
+
+  let zoom = d3
+    .zoom()
+    .extent([
+      [0, 0],
+      [width, height],
+    ])
+    .translateExtent([
+      [0, 0],
+      [width, height],
+    ])
+    .scaleExtent([1, 7])
+    .on("zoom", (event) => {
+      mapGroup.attr("transform", event.transform);
+    });
+
+  mapContainer.call(zoom);
 
   return svg;
 };
