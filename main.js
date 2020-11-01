e5306fae8e229d97b114f0a86329b1297321031e
Javier <jiaranda@uc.cl>
2020-11-01 12:54:32 -0300

    ✨ zoom and panning

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const main = async () => {
   const listingsData = await loadCSV("data/listings.csv");
   const neighborhoodsGeoData = await loadJSON("data/neighborhoods.geojson");
-  await createMap(neighborhoodsGeoData);
+  const mapContainer = d3.select("#map");
+  const mapSVG = await createMap(neighborhoodsGeoData);
+
+  let zoom = d3
+    .zoom()
+    .extent([
+      [0, 0],
+      [500, 500],
+    ])
+    .translateExtent([
+      [-100, -100],
+      [500 + 100, 500 + 100],
+    ])
+    .scaleExtent([1, 12])
+    .on("zoom", (event) => {
+      mapSVG.select("#mapGroup").attr("transform", event.transform);
+    });
+
+  mapContainer.call(zoom);
 };
 
 main();
--- map.js
+++ map.js
@@ -1,14 +1,26 @@
 const createMap = async (geoData) => {
-  const projection = d3.geoMercator().fitSize(["800", "500"], geoData);
+  const projection = d3.geoMercator().fitSize(["500", "500"], geoData);
   const geoPaths = d3.geoPath().projection(projection);
 
   const svg = d3
     .select("#map")
     .append("svg")
-    .attr("width", "100%")
+    .attr("width", "500")
     .attr("height", "500");
 
   svg
+    .append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", "500")
+    .attr("height", "500");
+
+  const mapGroup = svg
+    .append("g")
+    .attr("clip-path", "url(#clip)")
+    .attr("id", "mapGroup");
+
+  mapGroup
     .selectAll("path")
     .data(geoData.features)
     .enter()
@@ -17,5 +29,6 @@ const createMap = async (geoData) => {
     .attr("fill", "blue")
     .attr("opacity", 0.3)
     .attr("stroke", "blue");
+
   return svg;
 };
